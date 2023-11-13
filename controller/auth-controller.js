68a7004d1286d594f38931d27176348f2e81d09a
muhammadaafiff <muhammad.afif250302@gmail.com>
2023-11-13 22:11:42 +0700

    fix status and message response

--- controller/auth-controller.js
+++ controller/auth-controller.js
@@ -25,7 +25,7 @@ module.exports = {
           res.status(400).json({
             status: false,
             code: 400,
-            message: err.message,
+            message: "Invalid request",
           });
         }
       },
@@ -49,7 +49,7 @@ module.exports = {
       res.status(400).json({
         status: false,
         code: 400,
-        message: err.message,
+        message: "Invalid request",
       });
     }
   },
